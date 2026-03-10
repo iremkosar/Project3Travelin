3ea7071e87bb9c05881a8127a7ca5e26352575f6
İrem Koşar <99809817+iremkosar@users.noreply.github.com>
2026-03-10 03:19:31 +0300

    commit

--- Project3Travelin/Models/OpenAiService.cs
+++ Project3Travelin/Models/OpenAiService.cs
@@ -4,7 +4,7 @@
     {
         private readonly HttpClient _httpClient;
         private const string OpenAiUrl = "https://api.openai.com/v1/chat/completions";
-        private const string apiKey = "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";
+        private const string apiKey = "";
         public OpenAiService()
         {
             _httpClient = new HttpClient();
